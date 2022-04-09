142ad24a1c399e3ac91d88f1c075488793b0d873
Ryan Broadsword <rbroadsword@gmail.com>
2022-04-08 23:31:46 -0700

    add variables for each question and test in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-$
+$(document).ready(function() {
+  $("#dragForm").submit(function(event) {
+    event.preventDefault();
+    const dragName = $("input#dragName").val();
+    console.log(dragName);
+    const dragMom = parseInt($("#dragMom").val());
+    console.log(dragMom);
+    const dragAesthetic = parseInt($("#dragAesthetic").val());
+    console.log(dragAesthetic);
+    const trademark = $("input:radio[name=trademark]:checked").val();
+    console.log(trademark);
+    const competition = $("input:radio[name=competition]:checked").val();
+    console.log(competition);
+    const dragMovie = parseInt($("#dragMovie").val());
+    console.log(dragMovie);
+  })
+})
